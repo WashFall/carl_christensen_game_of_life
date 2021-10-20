b6cff44e544c158468e4c78ea942ee525ea82a4f
WashFall <carl.christensen98@gmail.com>
2021-10-20 17:46:55 +0200

    Solved it, they live

--- Assets/GameOfLife.cs
+++ Assets/GameOfLife.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class GameOfLife : ProcessingLite.GP21
 {
     GameCell[,] cells; //Our game grid matrix
+    GameCell[,] copyCells;
     float cellSize = 0.25f; //Size of our cells
     int numberOfColums;
     int numberOfRows;
@@ -20,6 +21,7 @@ public class GameOfLife : ProcessingLite.GP21
 
         //Initiate our matrix array
         cells = new GameCell[numberOfColums, numberOfRows];
+        copyCells = new GameCell[numberOfColums, numberOfRows];
 
         //Create all objects
 
@@ -31,6 +33,7 @@ public class GameOfLife : ProcessingLite.GP21
             {
                 //Create our game cell objects, multiply by cellSize for correct world placement
                 cells[x, y] = new GameCell(x * cellSize, y * cellSize, cellSize);
+                copyCells[x, y] = new GameCell(x * cellSize, y * cellSize, cellSize);
 
                 //Random check to see if it should be alive
                 if (Random.Range(0, 100) < spawnChancePercentage)
@@ -43,44 +46,15 @@ public class GameOfLife : ProcessingLite.GP21
 
     void Update()
     {
-        int aliveCheck;
 
         //Clear screen
         Background(0);
 
         //TODO: Calculate next generation
-        for (int i = 0; i < numberOfColums; i++)
-        {
-            for (int j = 0; j < numberOfRows; j++)
-            {
-                aliveCheck = 0;
-                int column = -1;
-                int row = -1;
-                
-                while (column != 2)
-                {
-                    while (row != 2)
-                        if (i + column < 0 || i + column > numberOfColums || j + row < 0 || j + row > numberOfRows)
-                        {
-                            continue;
-                        }
-                        else
-                        {
-                            if (cells[i + column, j + row].alive == true)
-                            {
-                                aliveCheck++;
-                                row++;
-                            }
-                        }
-
-                    column++;
-                }
-
-            }
-        }
+        Count();
 
         //TODO: update buffer
-
+        Buffer();
         //Draw all cells.
         for (int y = 0; y < numberOfRows; ++y)
         {
@@ -91,6 +65,82 @@ public class GameOfLife : ProcessingLite.GP21
             }
         }
     }
+
+    void Count()
+    {
+        int aliveCheck;
+        for (int i = 0; i < numberOfColums; i++)
+        {
+            for (int j = 0; j < numberOfRows; j++)
+            {
+                aliveCheck = 0;
+                int column = -1;
+                int row = -1;
+
+                while (column <= 1)
+                {
+                    while (row <= 1)
+                    {
+                        if (i + column < 1)
+                        {
+                            column++;
+                        }
+                        else if (i + column > numberOfColums - 1)
+                            break;
+
+                        if (j + row < 1)
+                        {
+                            row++;
+                        }
+                        else if (j + row > numberOfRows - 1)
+                            break;
+
+                        if (cells[i + column, j + row].alive == true)
+                        {
+                            aliveCheck++;
+                        }
+                        row++;
+                    }
+
+                    column++;
+                    row = -1;
+                }
+
+                if (cells[i, j].alive == true)
+                    aliveCheck--;
+
+                Debug.Log(aliveCheck);
+                if (aliveCheck < 2 && cells[i, j].alive == true)
+                {
+                    copyCells[i, j].alive = false;
+                }
+                else if(aliveCheck > 1 && aliveCheck < 4 && cells[i, j].alive == true)
+                {
+                    copyCells[i, j].alive = true;
+                }
+                else if (aliveCheck > 3 && cells[i, j].alive == true)
+                {
+                    copyCells[i, j].alive = false;
+                }
+                else if (aliveCheck == 3 && cells[i, j].alive == false)
+                {
+                    copyCells[i, j].alive = true;
+                }
+            }
+        }
+    }
+
+    void Buffer()
+    {
+        for(int i = 0; i < numberOfColums; i++)
+        {
+            for (int j = 0; j < numberOfRows; j++)
+            {
+                cells[i, j].alive = copyCells[i, j].alive;
+            }
+        }
+    }
+
 }
 
 //You will probebly need to keep track of more things in this class
