af85842da4bc6c01d185a6fa79310c7dd4cb5073
WashFall <carl.christensen98@gmail.com>
2021-10-20 15:47:47 +0200

    Some changes, not much progress

--- Assets/GameOfLife.cs
+++ Assets/GameOfLife.cs
@@ -2,94 +2,125 @@ using UnityEngine;
 
 public class GameOfLife : ProcessingLite.GP21
 {
-	GameCell[,] cells; //Our game grid matrix
-	float cellSize = 0.25f; //Size of our cells
-	int numberOfColums;
-	int numberOfRows;
-	int spawnChancePercentage = 15;
+    GameCell[,] cells; //Our game grid matrix
+    float cellSize = 0.25f; //Size of our cells
+    int numberOfColums;
+    int numberOfRows;
+    int spawnChancePercentage = 15;
 
-	void Start()
-	{
-		//Lower framerate makes it easier to test and see whats happening.
-		QualitySettings.vSyncCount = 0;
-		Application.targetFrameRate = 4;
+    void Start()
+    {
+        //Lower framerate makes it easier to test and see whats happening.
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 4;
 
-		//Calculate our grid depending on size and cellSize
-		numberOfColums = (int)Mathf.Floor(Width / cellSize);
-		numberOfRows = (int)Mathf.Floor(Height / cellSize);
+        //Calculate our grid depending on size and cellSize
+        numberOfColums = (int)Mathf.Floor(Width / cellSize);
+        numberOfRows = (int)Mathf.Floor(Height / cellSize);
 
-		//Initiate our matrix array
-		cells = new GameCell[numberOfColums, numberOfRows];
+        //Initiate our matrix array
+        cells = new GameCell[numberOfColums, numberOfRows];
 
-		//Create all objects
+        //Create all objects
 
-		//For each row
-		for (int y = 0; y < numberOfRows; ++y)
-		{
-			//for each column in each row
-			for (int x = 0; x < numberOfColums; ++x)
-			{
-				//Create our game cell objects, multiply by cellSize for correct world placement
-				cells[x, y] = new GameCell(x * cellSize, y * cellSize, cellSize);
+        //For each row
+        for (int y = 0; y < numberOfRows; ++y)
+        {
+            //for each column in each row
+            for (int x = 0; x < numberOfColums; ++x)
+            {
+                //Create our game cell objects, multiply by cellSize for correct world placement
+                cells[x, y] = new GameCell(x * cellSize, y * cellSize, cellSize);
 
-				//Random check to see if it should be alive
-				if (Random.Range(0, 100) < spawnChancePercentage)
-				{
-					cells[x, y].alive = true;
-				}
-			}
-		}
-	}
+                //Random check to see if it should be alive
+                if (Random.Range(0, 100) < spawnChancePercentage)
+                {
+                    cells[x, y].alive = true;
+                }
+            }
+        }
+    }
 
-	void Update()
-	{
-		//Clear screen
-		Background(0);
+    void Update()
+    {
+        int aliveCheck;
 
-		//TODO: Calculate next generation
+        //Clear screen
+        Background(0);
 
-		//TODO: update buffer
+        //TODO: Calculate next generation
+        for (int i = 0; i < numberOfColums; i++)
+        {
+            for (int j = 0; j < numberOfRows; j++)
+            {
+                aliveCheck = 0;
+                int column = -1;
+                int row = -1;
+                
+                while (column != 2)
+                {
+                    while (row != 2)
+                        if (i + column < 0 || i + column > numberOfColums || j + row < 0 || j + row > numberOfRows)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            if (cells[i + column, j + row].alive == true)
+                            {
+                                aliveCheck++;
+                                row++;
+                            }
+                        }
 
-		//Draw all cells.
-		for (int y = 0; y < numberOfRows; ++y)
-		{
-			for (int x = 0; x < numberOfColums; ++x)
-			{
-				//Draw current cell
-				cells[x, y].Draw();
-			}
-		}
-	}
+                    column++;
+                }
+
+            }
+        }
+
+        //TODO: update buffer
+
+        //Draw all cells.
+        for (int y = 0; y < numberOfRows; ++y)
+        {
+            for (int x = 0; x < numberOfColums; ++x)
+            {
+                //Draw current cell
+                cells[x, y].Draw();
+            }
+        }
+    }
 }
 
 //You will probebly need to keep track of more things in this class
 public class GameCell : ProcessingLite.GP21
 {
-	float x, y; //Keep track of our position
-	float size; //our size
+    float x, y; //Keep track of our position
+    float size; //our size
 
-	//Keep track if we are alive
-	public bool alive = false;
+    //Keep track if we are alive
+    public bool alive = false;
 
-	//Constructor
-	public GameCell(float x, float y, float size)
-	{
-		//Our X is equal to incoming X, and so forth
-		//adjust our draw position so we are centered
-		this.x = x + size / 2;
-		this.y = y + size / 2;
+    //Constructor
+    public GameCell(float x, float y, float size)
+    {
+        //Our X is equal to incoming X, and so forth
+        //adjust our draw position so we are centered
+        this.x = x + size / 2;
+        this.y = y + size / 2;
 
-		//diameter/radius draw size fix
-		this.size = size / 2;
-	}
+        //diameter/radius draw size fix
+        this.size = size / 2;
+    }
 
-	public void Draw()
-	{
-		//If we are alive, draw our dot.
-		if (alive)
-		{
-			//draw our dots
-			Circle(x, y, size);
-		}
-	}
+    public void Draw()
+    {
+        //If we are alive, draw our dot.
+        if (alive)
+        {
+            //draw our dots
+            Circle(x, y, size);
+        }
+    }
 }
